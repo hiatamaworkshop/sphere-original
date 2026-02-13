bffd69f681f33c4bf8aa2092e9204014ae0e8b96
hiatama <hiatamaworkshop@gmail.com>
2026-02-13 17:30:04 +0900

    gen-010完了

---
+++ start-daemon.bat
@@ -0,0 +1,10 @@
+@echo off
+cd /d "C:\Users\kazuh\Desktop\Various\programming\DockerFiles\sphere-original\phi-agent"
+set SPHERE_URL=http://localhost:3001
+set SPHERE_WS=ws://localhost:3001
+set OLLAMA_HOST=http://localhost:11434
+set OLLAMA_MODEL=gemma2:2b
+set LOADOUT=random
+set EVALUATE=true
+set RESPONSE=false
+node dist/index.js --daemon --sleep 5000 --cycles 3
